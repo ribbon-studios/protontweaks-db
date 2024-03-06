01451653dca265e3f7d013e1bd1dffc449b65be9
Cecilia Sanare <ceci@sanare.dev>
2024-03-06 09:57:50 -0600

    fix: improve responsiveness of tweak page on mobile

--- app/components/Label.tsx
+++ app/components/Label.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export const Label: FC<Props> = ({ label, children }) => {
   return (
-    <div className="flex gap-4 items-center">
+    <div className="flex gap-4 items-start justify-between">
       <div className="bg-white/20 p-2 rounded-md mr-auto">{label}</div>
-      {children}
+      <div className="flex flex-wrap gap-4 items-center justify-end h-full">{children}</div>
     </div>
   );
 };
